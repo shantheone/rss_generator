763cf106b5bce56c6041c93a956ccebd5af7fd64
shan <shandoor@gmail.com>
2021-08-08 16:30:50 +0200

    Crude RSS generator finished TODO: how to fetch only new articles in the future

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 import datetime
 import urllib3
-import cssutils
-from rfeed import *
+# import cssutils
+
+from feedgen.feed import FeedGenerator
 from bs4 import BeautifulSoup
 
-# item1 = Item(
-#     title="Napi Retro",
-#     link="https://retro.land/napi-retro",
-#     description="This is the description of the first article",
-#     author="Shan",
-#     guid=Guid("https://retro.land/napi-retro"),
-#     pubDate=datetime.datetime(2021, 8, 7, 10, 00))
-#
-# feed = Feed(
-#     title="Sample RSS Feed",
-#     link="http://www.example.com/rss",
-#     description="This is an example of how to use rfeed to generate an RSS 2.0 feed",
-#     language="en-US",
-#     lastBuildDate=datetime.datetime.now(),
-#     items=[item1])
 
 if __name__ == '__main__':
-    # print(feed.rss())
-
     # urllib3
     http = urllib3.PoolManager()
 
@@ -33,25 +17,42 @@ if __name__ == '__main__':
     # run it through the html.parser
     soup = BeautifulSoup(html_text, 'html.parser')
 
-    # Find articlebox css class (this is where the articles are stored)
+    # find articlebox css class (this is where the articles are stored)
     div_mainpage = soup.findAll('div', class_='articleBox')
+
+    # create feedgenerator
+    fg = FeedGenerator()
+    fg.id(feed_url)
+    fg.title('Retro Land')
+    fg.link(href='https://retro.land', rel='alternate')
+    fg.subtitle('Retro Land')
+    fg.link(href='https://ds.sandorkovacs.hu/retroland.xml', rel='self')
+    fg.language('en')
 
     # iterate through all of the elements
     for element in div_mainpage:
-        print()
+        # add each new item to the feed
+        fe = fg.add_entry()
+
         title = element.find('a', class_='title')
-        print('Title     :', title.text)
-        print('Scanned   :', datetime.datetime.now())
 
         # for the image links we'll have to use the cssutils module
-        image = element.find('a')['style']
-        style = cssutils.parseStyle(image)
-        image_url = style['background-image']
+        # image = element.find('a')['style']
+        # style = cssutils.parseStyle(image)
+        # image_url = style['background-image']
         # adding the feed_url to the beginning of the image link so it will work correctly in the feed
-        image_url = image_url.replace('url(', feed_url).replace(')', '')
-        print('Image url :', image_url)
+        # image_url = image_url.replace('url(', feed_url).replace(')', '')
+        # print('Image url :', image_url)
+
+        article_url = element.find('a', class_='more', href=True)
+        article_url = feed_url + article_url['href']
 
         lead = element.find('p', class_='lead')
-        print('Lead      :', lead.text)
 
-        # results.append(title.text)
+        fe.title(title.text)
+        fe.description(lead.text)
+        # fe.id(article_url)
+        fe.link(href=article_url)
+
+fg.rss_file('rss.xml')  # Write the RSS feed to a file
+
