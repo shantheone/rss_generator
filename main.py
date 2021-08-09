44c57db6d2183729fc432969d8f0f6885fc543bb
shan <shandoor@gmail.com>
2021-08-09 22:08:14 +0200

    - main site feed complete with images - TODO: /napi-retro feed

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import datetime
 import urllib3
-# import cssutils
+import cssutils
 
 from feedgen.feed import FeedGenerator
 from bs4 import BeautifulSoup
 
 
-if __name__ == '__main__':
+def retroland_main():
     # urllib3
     http = urllib3.PoolManager()
 
@@ -23,11 +23,10 @@ if __name__ == '__main__':
     # create feedgenerator
     fg = FeedGenerator()
     fg.id(feed_url)
-    fg.title('Retro Land')
+    fg.title('Retro Land - Nagy Cikkek')
     fg.link(href='https://retro.land', rel='alternate')
     fg.subtitle('Retro Land')
-    fg.link(href='https://ds.sandorkovacs.hu/retroland.xml', rel='self')
-    fg.language('en')
+    fg.language('hu')
 
     # iterate through all of the elements
     for element in div_mainpage:
@@ -35,6 +34,57 @@ if __name__ == '__main__':
         fe = fg.add_entry()
 
         title = element.find('a', class_='title')
+
+        # for the image links we'll have to use the cssutils module
+        image = element.find('a')['style']
+        style = cssutils.parseStyle(image)
+        image_url = style['background-image']
+        # adding the feed_url to the beginning of the image link so it will work correctly in the feed
+        image_url = image_url.replace('url(', '').replace(')', '')
+
+        # creating article url
+        article_url = element.find('a', class_='more', href=True)
+        article_url = feed_url + article_url['href']
+
+        lead = element.find('p', class_='lead')
+
+        fe.title(title.text)
+        fe.description("<p><img src=" + '"' + image_url + '"' + "/></p>" + lead.text)
+        # print("<p><img src=" + '"' + image_url + '"' + "/></p>" + lead.text)
+        # fe.enclosure(url=feed_url + image_url, type="image")
+        fe.link(href=article_url)
+
+    fg.rss_file('retroland.xml')  # Write the RSS feed to a file
+
+
+def retroland_napi():
+    # urllib3
+    http = urllib3.PoolManager()
+
+    # url to generate feed from
+    feed_url = 'https://retro.land/napi-retro'
+    # get contents of webpage in utf-8
+    html_text = http.request('GET', feed_url).data.decode('utf-8')
+    # run it through the html.parser
+    soup = BeautifulSoup(html_text, 'html.parser')
+
+    # find articlebox css class (this is where the articles are stored)
+    div_dailylist = soup.findAll('div', id='dailyList')
+
+    # create feedgenerator
+    fg = FeedGenerator()
+    fg.id(feed_url)
+    fg.title('Retro Land - Napi Retro')
+    fg.link(href='https://retro.land/napi-retro', rel='alternate')
+    fg.subtitle('Retro Land - Napi Retro')
+    fg.language('hu')
+
+    # iterate through all of the elements
+    for element in div_dailylist:
+        # add each new item to the feed
+        fe = fg.add_entry()
+
+        title = "retro.land - napi retro cikk"
 
         # for the image links we'll have to use the cssutils module
         # image = element.find('a')['style']
@@ -52,7 +102,12 @@ if __name__ == '__main__':
         fe.title(title.text)
         fe.description(lead.text)
         # fe.id(article_url)
+        # fe.enclosure(url='"' + image_url + '"', type="image")
         fe.link(href=article_url)
 
-fg.rss_file('rss.xml')  # Write the RSS feed to a file
+    fg.rss_file('rss.xml')  # Write the RSS feed to a file
 
+
+if __name__ == '__main__':
+    retroland_main()
+    # retroland_napi()
