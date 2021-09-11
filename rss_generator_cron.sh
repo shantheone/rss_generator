c4712ec5d1728585cb9aa4d3e704f63ccdf2c671
Sándor Kovács <shandoor@gmail.com>
2021-09-11 10:30:44 +0200

    Fixed some path related errors in rss_generator_cron.sh

--- rss_generator_cron.sh
+++ rss_generator_cron.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 source /var/services/homes/shan/scripts/rss_generator/.venv/bin/activate
+cd /var/services/homes/shan/scripts/rss_generator/
 python main.py
 deactivate
 mv *.xml /volume1/web/rss/
